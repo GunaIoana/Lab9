4f4676e4072ddfc15b868af11872e252dad66d48
GunaIoana <gunaia@mail.uc.edu>
2020-11-13 11:12:28 +0000

    ..

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+
+
+class Main {
+  public static void main(String[] args) {
+    ArrayList<Person> people = new ArrayList<Person>();
+    String name = "", age = "", color = "";
+
+    people.add(new Person("Ioana", 21, "Green"));
+    people.add(new Person("A", 87, "Pink"));
+    people.add(new Person("B", 13, "Yellow"));
+    people.add(new Person("C", 55, "Black"));
+    people.add(new Person("D", 4, "Green"));
+
+    try {
+      FileWriter toWriteFile;
+      toWriteFile = new FileWriter("data.txt");
+      BufferedWriter output = new BufferedWriter(toWriteFile);
+
+      for(int i=0; i<people.size(); i++) {
+        output.write(people.get(i).getName());
+        output.newLine();
+        output.write(Integer.toString(people.get(i).getAge()));
+        output.newLine();
+        output.write(people.get(i).getColor());
+        output.newLine();
+        output.flush();
+      }
+      output.close();
+    }
+    catch (IOException exception) {
+      System.out.println("An error occurred: " + exception);
+    }
+
+    try {
+      FileReader myFile;
+      myFile = new FileReader("data.txt");
+      BufferedReader reader = new BufferedReader(myFile);
+
+      while(reader.ready()) {
+        name = reader.readLine();
+        age = reader.readLine();
+        color = reader.readLine();
+
+        System.out.printf("%-10s %-10s %-10s %n", name, age, color);
+      }
+      reader.close();
+    } catch (IOException exception) {
+      System.out.println("An error occured: " + exception);
+    }
+  }
+}
